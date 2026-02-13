099188d8c07e91f050aa4f280949c454129f9737
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-02-13 17:16:25 -0300

    Service layer, component registration

--- src/main/java/com/vrzada/webservices/config/TestConfig.java
+++ src/main/java/com/vrzada/webservices/config/TestConfig.java
@@ -22,9 +22,10 @@ public class TestConfig implements CommandLineRunner{
 		//classe de configuração para instanciar o banco de dados
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "Vitor", "vrzada@gmail.com", "7777777","444444");
 		
 		//cria uma lista a partir dos objetos u1 e u2 e adiciona no banco de dados
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		
 		
 	}
---
+++ src/main/java/com/vrzada/webservices/services/UserService.java
@@ -0,0 +1,25 @@
+package com.vrzada.webservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vrzada.webservices.entities.User;
+import com.vrzada.webservices.repositories.UserRepository;
+
+@Service //registra a classe como um componente de serviço do Spring
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
