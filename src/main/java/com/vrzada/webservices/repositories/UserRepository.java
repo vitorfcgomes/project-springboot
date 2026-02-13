8070d27e169fc433778626a4b597c78c2dbda182
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-02-13 15:31:54 -0300

    JPA Repository, depency injection, database seeding

---
+++ src/main/java/com/vrzada/webservices/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vrzada.webservices.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vrzada.webservices.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
