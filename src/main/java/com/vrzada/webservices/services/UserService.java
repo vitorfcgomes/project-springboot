8e7e2e31a44609d5966118e8f1bb5048b9778200
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-02-19 17:23:12 -0300

    Exception hadling Update

--- src/main/java/com/vrzada/webservices/services/UserService.java
+++ src/main/java/com/vrzada/webservices/services/UserService.java
@@ -12,6 +12,8 @@ import com.vrzada.webservices.repositories.UserRepository;
 import com.vrzada.webservices.services.exceptions.DatabaseException;
 import com.vrzada.webservices.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service // registra a classe como um componente de serviço do Spring
 public class UserService {
 	@Autowired
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
