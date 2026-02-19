143c8c60841511a59fffade9ad69861812e290c4
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-02-19 13:56:51 -0300

    Subtotal and Total Methods

--- src/main/java/com/vrzada/webservices/entities/OrderItem.java
+++ src/main/java/com/vrzada/webservices/entities/OrderItem.java
@@ -65,6 +65,11 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
